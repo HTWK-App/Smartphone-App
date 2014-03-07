d265a85c61141b32b37495cb06b7ed3a23e25451
Zipunrar <zipunrar@gmail.com>
2014-03-07 14:27:52 +0100

    Big Optimization and Bugfixing Update
    What's New:
    
    - Source Code is optimized and restructured
    - "Ready-to-Use"-Libaries like jQuery are now in js/libs
    - Use config.js for general Settings and global Constants
    - Use fn.js for small functions with non-specific intention
    - Base Function of all Menupoint (except Settings) working
    
    What TODO next:
    
    - CSS!
    - Page Refresh & Loading-Spinner Bugfix
    - Google Maps in Building (pretty ugly yet)
    - Settings
    - small other Things (see Source Code Comments)
    - Remove old Source Code (actual commented out for Learning and
      direct Comparison to new Source)
    - Localization (would be german and english for the beginning
      ... only a bit really ^^)
    - Upgrade to new jQuery Mobile Version (note Changelog when updating!)
    
    Puh, that's it for now. Doing more in some days...

---
+++ www/js/fn.js
@@ -0,0 +1,87 @@
+/**
+ * General Function Libary for small, "Every Day"-Work.
+ * 
+ * @author Hubertus Willuhn
+ * @version 1.0
+ * 
+ * UTF-8
+ */
+
+// Capitalize first Letter in String
+function capitalize(str) {
+	return str.charAt(0).toUpperCase() + str.slice(1);
+};
+
+// Test if val is empty (for all Types)
+function isEmpty(val) {
+	return (val === "" || 
+			val === {} || 
+			val === null || 
+			val === undefined || 
+			(val instanceof Array && val.length === 0));
+};
+
+// Creates a DateStamp for Server Requests, Format: YYYYMMDD
+function createServerDateStamp(date) {
+	if(isEmpty(date)) date = new Date();
+	
+	var dd = date.getDate();
+	var mm = date.getMonth()+1; //January is 0!
+	var yyyy = date.getFullYear();
+	
+	if(dd<10) dd="0"+dd;
+	if(mm<10) mm="0"+mm;
+	
+	return yyyy+""+mm+""+dd;
+};
+
+//Creates a DateStamp for general Purposes, Format: WeedDay, dd.mm.yyyy
+function createDateStamp(date) {
+	if(isEmpty(date)) date = new Date();
+	
+	var dd = date.getDate();
+	var mm = date.getMonth()+1; //January is 0!
+	var yyyy = date.getFullYear();
+	
+	if(dd<10) dd="0"+dd;
+	if(mm<10) mm="0"+mm;
+	
+	return GLOBAL.DATE.weekdays[date.getDay()].name+', '+dd+'.'+mm+'.'+yyyy;
+};
+
+// Creates a DateTimeStamp, Format: dd.mm.yyyy hh:mm:ss
+function createDateTimeStamp(date) {
+	if(isEmpty(date)) date = new Date();
+	
+	var dd = date.getDate();
+	var mm = date.getMonth()+1; //January is 0!
+	var yyyy = date.getFullYear(); 
+	var hh = date.getHours();
+	var mi = date.getMinutes();
+	var ss = date.getSeconds();
+	
+	if(dd<10) dd="0"+dd;
+	if(mm<10) mm="0"+mm;
+	if(hh<10) hh="0"+hh;
+	if(mi<10) mi="0"+mi;
+	if(ss<10) ss="0"+ss;
+	
+	return (dd+"."+mm+"."+yyyy+" "+hh+":"+mi+":"+ss);	
+};
+
+// Escaping HTML Special Chars like & or <>
+function escape(str) {
+	return str.replace(/&/g, "&amp;")
+			  .replace(/</g, "&lt;")
+			  .replace(/>/g, "&gt;")
+			  .replace(/"/g, "&quot;")
+			  .replace(/'/g, "&#039;");
+};
+
+function loadingIn() {
+	$.mobile.loading("show");
+};
+
+function loadingOut() {
+	$.mobile.loading("hide");
+};
