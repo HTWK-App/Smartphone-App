8977eb9bbe5c767e2e46b990273ee6a62b250806
Roy Meissner <roymeissn@googlemail.com>
2014-04-09 12:59:15 +0200

    Refactored Gmaps, added some error messages

--- www/js/gmaps.js
+++ www/js/gmaps.js
@@ -1,57 +1,49 @@
-			var fromLat;
-			var fromLng;
-			var toLat;
-			var toLng;
-			var mapcontainer;
-			var map;
+var GMAPS = {
+	destination : [],
+	mapcontainer : null,
 
-			var directionsDisplay;
-			var directionsService;
+	initGmap : function(dest, cont){
+		GMAPS.destination = dest;
+		GMAPS.mapcontainer = cont;
 
-			function onGeoGetSuccess (p){
-//				alert("geosuccess");
-				fromLat = parseFloat(p.coords.latitude);
-				fromLng = parseFloat(p.coords.longitude);
-				calcRoute();
-				
-			}
-			function onGeoGetFail(e){
-				alert("geofail");
-			}
-			function calcRoute() {
-				//alert("calcRoute");
-				start = new google.maps.LatLng(fromLat, fromLng);
-				end = new google.maps.LatLng(toLat, toLng);
-				var request = {
-					origin: start,
-					destination: end,
-					travelMode: google.maps.TravelMode.WALKING
-				};
-				directionsService.route(request, function(response, status) {
-					console.log(response);
-					if (status == google.maps.DirectionsStatus.OK) {
-					  directionsDisplay.setDirections(response);
-					}
-				});
-			}
-			function initGmap(){
-				//alert("initGmap");
-				navigator.geolocation.getCurrentPosition(onGeoGetSuccess, onGeoGetFail,{timeout:30000,enableHighAccuracy: true, maximumAge: 0});
-				//alert("end init");
-				directionsService = new google.maps.DirectionsService();
+		navigator.geolocation.getCurrentPosition(GMAPS.onGeoGetSuccess, GMAPS.onGeoGetFail, {timeout:30000, maximumAge: 7000, enableHighAccuracy: true});
+	},
 
-				var mapOptions = {
-					zoom: 7,
-					disableDefaultUI : true,
-					draggable : false,
-					keyboardShortcuts : false,
-					scrollwheel : false,
-					mapTypeId: google.maps.MapTypeId.WALKING
-				}				
-				map = new google.maps.Map(mapcontainer.find(".map-canvas")[0], mapOptions);
-				var rendererOptions = {
-					map: map
-				}				
-				directionsDisplay = new google.maps.DirectionsRenderer(rendererOptions);
-				
+	onGeoGetFail : function(e){
+		GMAPS.mapcontainer.find(".map-canvas").parent().empty().append("<p>Eine Karte kann nicht angezeigt werden, da die Ortsbestimmung fehgeschlagen ist.</p>");
+	},
+
+	onGeoGetSuccess : function(pos){
+
+		GMAPS.calcRoute([parseFloat(pos.coords.latitude), parseFloat(pos.coords.longitude)]);	
+	},
+
+	calcRoute : function(position) {
+
+		var directionsService = new google.maps.DirectionsService();
+		var mapOptions = {
+			zoom: 7,
+			disableDefaultUI : true,
+			draggable : false,
+			keyboardShortcuts : false,
+			scrollwheel : false,
+			mapTypeId: google.maps.MapTypeId.WALKING
+		}
+		var map = new google.maps.Map(GMAPS.mapcontainer.find(".map-canvas")[0], mapOptions);
+		var directionsDisplay = new google.maps.DirectionsRenderer({map : map});
+		var start = new google.maps.LatLng(position[0], position[1]);
+		var end = new google.maps.LatLng(GMAPS.destination[0], GMAPS.destination[1]);
+		var request = {
+			origin: start,
+			destination: end,
+			travelMode: google.maps.TravelMode.WALKING
+		};
+
+		directionsService.route(request, function(response, status) {
+
+			if (status == google.maps.DirectionsStatus.OK) {
+				directionsDisplay.setDirections(response);
 			}
+		});
+	}
+}
