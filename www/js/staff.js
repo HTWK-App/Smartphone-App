ee707a15ef03b5fc647582e2648c1b8b0bc3decd
Roy Meissner <roymeissn@googlemail.com>
2014-05-26 19:32:04 +0200

    Design changes

--- www/js/staff.js
+++ www/js/staff.js
@@ -14,40 +14,39 @@ function loadStaffDetails(id) {
 				{ ajaxErrorHandler(data, status, jqXHR); return; }
 
 			var name = data.name.split(", ");
-			var div = 	'<div id="staff_'+id+'" data-role="page" data-theme="b" data-subpage="true" data-title="Mitarbeiter & Professoren" class="staffdetails">'+
-							'<div data-role="header" data-position="fixed" data-tap-toggle="false" data-theme="b" class="header">'+
-								'<a href="#" data-rel="back" data-role="button" data-transition="slide" data-direction="reverse"><span class="icon-angle-left"></span></a>'+
-								'<h1>HTWK App</h1>'+
-							'</div>'+
-					'<div data-role="content">'+
-					'	<div class="inset">'+
-					'		<h1>'+data.degree+' '+name[1]+' '+name[0]+'</h1>'+
-					'		<ul data-nativedroid-plugin="cards" class="nativeDroidCards">'+
-					'			<li data-cards-type="text">'+
-					'				<!--<h2>Info</h2> BILD -->'+
-					'				<img src="'+data.pictureData+'"/>'+
-					'			</li>'+
-					'			<li data-cards-type="text">'+
-					'				<h1>Info</h1><hr/>'+
-					'				<strong>Grad:</strong> '+data.degree+'<br/>'+
-					'				<strong>Bereich:</strong> '+data.faculty+'<br/>'+
-					'				<strong>Objekt:</strong> '+data.location+'<br/>'+
-					'				<p>'+data.description+'</p>'+
-					'			</li>'+
-					'			<li data-cards-type="text">'+
-					'				<h1>Kontakt</h1><hr/>'+
-					'				<strong>E-Mail:</strong> <a href="mailto:'+data.email+'">'+data.email+'</a><br/>'+
-					'				<strong>Telefon:</strong> <a href="tel:'+data.telephone+'">'+data.telephone+'</a><br/>'+
-					'				<strong>Fax:</strong><hr/> '+data.telefax+
-					'			</li>'+
-					'			<li data-cards-type="text">'+
-					'				<h1>Links</h1><hr/>'+
-					'				<strong><a href="'+data.detailLink+'" class="external">Details zur Person</a></strong> '+
-					'				<strong><a href="'+data.vcardLink+'" class="external">V-Card</a></strong>'+
-					'			</li>'+
-					'		</ul>'+
-					'	</div>'+
-					'</div>';
+			var div = '<div id="staff_'+id+'" data-role="page" data-theme="b" data-subpage="true" data-title="Mitarbeiter & Professoren" class="staffdetails">'+
+								'	<div data-role="header" data-position="fixed" data-theme="b" data-tap-toggle="false" class="header">'+
+								'		<a href="#" data-rel="back" class="ui-btn-left ui-btn ui-btn-inline ui-mini ui-corner-all"><i class="fa fa-reply fa-lg"></i></a>'+
+								'		<h1>HTWK App</h1>'+
+								'	</div>'+
+								'	<div role="main" class="ui-content">'+
+								'		<h1>'+data.degree+' '+name[1]+' '+name[0]+'</h1>'+
+								'		<ul class="nativeDroidCards">'+
+								'			<li>'+
+								'				<!--<h2>Info</h2> BILD -->'+
+								'				<img src="'+data.pictureData+'"/>'+
+								'			</li>'+
+								'			<li>'+
+								'				<h1>Info</h1><hr/>'+
+								'				<strong>Grad:</strong> '+data.degree+'<br/>'+
+								'				<strong>Bereich:</strong> '+data.faculty+'<br/>'+
+								'				<strong>Objekt:</strong> '+data.location+'<br/>'+
+								'				<p>'+data.description+'</p>'+
+								'			</li>'+
+								'			<li>'+
+								'				<h1>Kontakt</h1><hr/>'+
+								'				<strong>E-Mail:</strong> <a href="mailto:'+data.email+'" class="ui-btn ui-btn-b ui-corner-all ui-mini ui-shadow">'+data.email+'</a><br/>'+
+								'				<strong>Telefon:</strong> <a href="tel:'+data.telephone+'" class="ui-btn ui-btn-b ui-corner-all ui-mini ui-shadow">'+data.telephone+'</a><br/>'+
+								'				<strong>Fax:</strong><br/><strong>'+data.telefax+'</strong>'+
+								'			</li>'+
+								'			<li>'+
+								'				<h1>Links</h1><hr/>'+
+								'				<a href="'+data.detailLink+'" class="ui-btn ui-btn-b ui-corner-all ui-mini ui-shadow external">Details zur Person</a>'+
+								'				<a href="'+data.vcardLink+'" class="ui-btn ui-btn-b ui-corner-all ui-mini ui-shadow external">V-Card</a>'+
+								'			</li>'+
+								'		</ul>'+
+								'	</div>'+
+								'</div>';
 		
 			div = $(div).on("pagehide", function (){$(this).remove();});
 
