ba0b57b87603caf9ca81ca6e7ec4724c758f3396
Roy Meissner <roy-meissner@gmx.net>
2016-07-19 20:09:09 +0200

    Minify + minor changes

--- Docker Debug Build/dockerDeploy.sh
+++ Docker Debug Build/dockerDeploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build -t cordova/htwk-app ./
+sudo ocker build -t cordova/htwk-app ./
--- Docker Debug Build/dockerStart.sh
+++ Docker Debug Build/dockerStart.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -it --rm  -v ~/.git/Smartphone-App/:/home/cordova/Smartphone-App/ cordova/htwk-app
+docker run -it --rm  -v ../:/home/cordova/Smartphone-App/ cordova/htwk-app
